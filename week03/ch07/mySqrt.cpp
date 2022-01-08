413f432234a52f9520cfcc45bbb690d709b6f92f
ZhangXiangyu <884188766@qq.com>
2022-01-08 20:36:01 +0800

    Sqrt(x)

---
+++ week03/ch07/mySqrt.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+
+int mySqrt(int x)
+{
+
+	int left=1;
+	int right=x;
+	int mid;
+	int sqrt;
+	if(x==0)
+	{
+		return 0;
+	}
+	while(left<=right)
+	{
+		mid=left+(right-left)/2;
+		sqrt=x/mid;
+		if(sqrt==mid)
+		{
+			return mid;
+		}
+		if(sqrt<mid)
+		{
+			right=mid-1;
+		}
+		if(sqrt>mid)
+		{
+			left=mid+1;
+		}
+		
+	}
+	return left-1;
+}
+
+
+
+
+
+
+int main()
+{
+	printf("%d\n",mySqrt(8));
+}
