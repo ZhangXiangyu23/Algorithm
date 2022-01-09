0ecede6a80f5bde4fd64c8927e760618f12e496d
ZhangXiangyu <884188766@qq.com>
2022-01-09 11:22:07 +0800

    python

---
+++ week03/01_Python基础/hm_04_qq号码.py
@@ -0,0 +1,7 @@
+# 定义qq号码的变量
+qq_number = "123456"
+qq_password = "123"
+
+# 输出变量需要使用print函数，不像在终端的ipython中，直接写上变量名就行
+print(qq_number)
+print(qq_password)
---
+++ week03/01_Python基础/hm_05_超市买苹果.py
@@ -0,0 +1,14 @@
+# 定义单价
+price = 8.5
+
+# 定义重量
+weight = 7.5
+
+# 计算总价
+money = price * weight
+
+# 买苹果，返5元红包
+money = money - 5
+
+# 输出总价
+print(money)
